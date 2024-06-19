54d0d32c8365ef1bd05bd5f57cab429f8f5dc20b
mathieu.gaisnon <mathieu.gaisnon@lycee-elie-vinet.org>
2024-06-19 14:25:40 +0200

    toolbar

--- app/src/main/java/com/example/rickandmortyapp/CharacterActivity.kt
+++ app/src/main/java/com/example/rickandmortyapp/CharacterActivity.kt
@@ -1,27 +1,42 @@
 package com.example.rickandmortyapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.rickandmortyapp.data.Character
 import com.example.rickandmortyapp.network.RetrofitInstance
+import com.google.android.material.appbar.MaterialToolbar
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class CharacterActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_character)
-        enableEdgeToEdge()
+
 
         val characterId = intent.getIntExtra("character_id", -1)
         Log.d("CharacterDetailActivity", "Received Character ID: $characterId")
 
+        val button : ImageView = findViewById(R.id.retour)
+
+        button.setOnClickListener{
+            val context = button.context
+            val intent = Intent(context,MainActivity::class.java)
+            context.startActivity(intent)
+
+
+        }
         if (characterId != -1) {
             fetchCharacterDetails(characterId)
         } else {
@@ -37,6 +52,7 @@ class CharacterActivity : AppCompatActivity() {
                 displayCharacterDetails(response)
             } catch (e: Exception) {
                 // Handle exception
+                Log.d("Error", "Erreur lors du chargement")
             }
         }
     }
@@ -48,8 +64,9 @@ class CharacterActivity : AppCompatActivity() {
         val characterStatus : TextView = findViewById(R.id.statut)
         val characterSpecies : TextView = findViewById(R.id.espece)
         val characterImage : ImageView = findViewById(R.id.picture)
+        val toolbar : MaterialToolbar = findViewById(R.id.toolbardetail)
 
-
+        toolbar.title = character.name
         characterName.text = character.name
         characterOrigin.text = character.origin.name
         characterLocation.text = character.location.name
